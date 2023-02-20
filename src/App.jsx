97a808254db471c6afe59481b6be0eefea8cefa1
WYunH <helvetaw@gmail.com>
2023-02-20 22:56:51 +0900

    test2:test2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-TEST
+TEST2
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
