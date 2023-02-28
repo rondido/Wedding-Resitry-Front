fdfb92e8b50607cf69e706d278cac394039d452a
WYunH <helvetaw@gmail.com>
2023-02-28 15:59:39 +0900

    [feature/WEDPRO-19] admin 관련 사용 요소 임시 적용

--- src/pages/AdminMemo.jsx
+++ src/pages/AdminMemo.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function AdminMemo() {
   return (
-    <div>url 등록 / 메모 CRUD 공간</div>
+    <div>
+      <div>
+        <input placeholder='url을 입력해주세요' />
+        <p>상품 이름: </p>
+        <p>상품 가격: </p>
+        <button>수정하기</button>
+        <button>삭제하기</button>
+      </div>
+      <div>
+        <p>메모 CRUD 공간</p>
+        <button>저장하기</button>
+      </div>
+      </div>
   )
 }
 
