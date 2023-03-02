6bfd8fb71be2ecacf0a09976fb3b46260318ae34
WYunH <helvetaw@gmail.com>
2023-03-02 16:55:30 +0900

    [feature/WEDPRO-21] 참석여부 정렬 기능 적용

--- src/pages/AdminAttendanceLists.jsx
+++ src/pages/AdminAttendanceLists.jsx
@@ -1,41 +1,38 @@
 import React, {useState} from 'react'
-import { ReactSortable } from "react-sortablejs";
 
+import AdminLists from '../components/AdminLists'
 // 결혼식 참석 여부 리스트 페이지
 function AdminAttendanceLists() {
 
-  const [state, setState] = useState([
+  const [attendance, setAttendance] = useState([
     { id: 1, name: "김미경" },
     { id: 2, name: "조수민" },
+  ]);
+
+  const [absence, setAbsence] = useState([
     { id: 3, name: "이인아" },
     { id: 4, name: "한민지" },
     { id: 5, name: "수민우" },
     { id: 6, name: "권민오" },
+  ]);
+
+  const [undecided, setUndecided] = useState([
     { id: 7, name: "조지훈" },
     { id: 8, name: "최우림" },
   ]);
 
   return (
     <div>
-      <div>
+
         <h3>결혼식 참석 여부</h3>
         <p>차트 그래프</p>
 {/*     <p>결혼식 참석</p>
         <p>%, 명</p>
         <p>결혼식 불참석</p>
         <p>%, 명</p> */}
-      </div>
 
-      <div>
       <h3>참석자</h3>
-      <ReactSortable list={state} setList={setState} sort="true" animation="200" easing="ease-out">
-      {state.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
-    </ReactSortable>
-      </div>
-      <h3>불참자</h3>
-      <h3>미정</h3>
+      <AdminLists attendance={attendance} setAttendance={setAttendance} absence={absence} setAbsence={setAbsence} undecided={undecided} setUndecided={setUndecided} />
   </div>
   )
 }
