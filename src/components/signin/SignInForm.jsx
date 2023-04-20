9d953d72d30532e7d936b965a7204c5d14fec373
WYunH <helvetaw@gmail.com>
2023-04-20 14:05:01 +0900

    [feature/WEDPRO-52] design: 기본 로그인 CSS

--- src/components/signin/SignInForm.jsx
+++ src/components/signin/SignInForm.jsx
@@ -1,16 +1,61 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-const StyledInput = styled.input`
+const StyledWrapper = styled.div`
+width: 100%;
+  min-width: 344px;
+  margin-top: 4rem;
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
+  .left{
+    margin-top: .5rem;
+    width: 38%;
+    justify-self: flex-start;
+  }
+`
+
+const StyledInput = styled.input`
+  min-width: 344px;
+  min-height: 44px;
+  margin-bottom: .5rem;
+  width: 30%;
+  border-radius: 10px;
+  padding-left: .8rem;
+`;
+
+
+const StyledDiv = styled.div`
+ display: flex;
+  min-width: 344px;
+  width: 38%;
+  
+  justify-content: center;
+  align-items: center;
+  span:first-child {
+    align-self: flex-start;
+    flex-grow: 1;
+  }
+  span:nth-child(2) {
+    margin-right: 1rem;
+  }
 `;
 
 const StyledButton = styled.button`
-
-`;
-
-const StyledDiv = styled.div`
- 
+  width: 38%;
+  min-height: 44px;
+  min-width: 344px;
+  border-radius: 10px;
+  color: #fff;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  background-color: #000000B2;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  margin-top: 1.5em;
 `;
 
 const StyledSpan = styled.span`
@@ -43,7 +88,7 @@ function SignInForm() {
     }
 
     return (
-        <>
+        <StyledWrapper>
             <StyledInput
                 type="text"
                 name="email"
@@ -63,8 +108,15 @@ function SignInForm() {
                 <StyledSpan>아이디 찾기</StyledSpan>
                 <StyledSpan>비밀번호 찾기</StyledSpan>
             </StyledDiv>
-            <StyledButton onClick={onClickButton}>로그인하기</StyledButton>
-        </>
+            <StyledButton onClick={onClickButton}>로그인 하기</StyledButton>
+            <div className="left">
+            <input type={"checkbox"} id="remember-signin"/>
+            <label htmlFor="remember-signin">
+                로그인 상태 기억하기
+            </label>
+
+            </div>
+        </StyledWrapper>
     );
 }
 
--- src/containers/signin/SignInContainer.jsx
+++ src/containers/signin/SignInContainer.jsx
@@ -6,15 +6,18 @@ import KakaoSignInButton from "../../components/signin/KakaoSignInButton.jsx";
 
 const SignInStyled = styled.div`
   background: rgba(234, 234, 234, 0.4);
-  margin: auto;
   
   div.wrapper {
+  margin: 5em auto 5rem;
+  display: flex;
+  flex-direction: column; 
   opacity: 0.7;
-  border-radius: 40px;
+  border-radius: 3rem;
   border: 1px solid rgba(0, 0, 0, 0.4);
   box-shadow: 3px 4px 4px rgba(0, 0, 0, 0.25);
-  height: 573px;
-  width: 653px;
+  min-height: 573px;
+  width: 50%;
+  min-width: 653px;
   }
 
   
