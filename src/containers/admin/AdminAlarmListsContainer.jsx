7898e8d2787de1bf9dd8f7e2ca7f0c5f0767b2ff
YunHeeW <helvetaw@gmail.com>
2023-07-19 20:38:29 +0900

    feat: 알림 목록 페이지 api 적용

--- src/containers/admin/AdminAlarmListsContainer.jsx
+++ src/containers/admin/AdminAlarmListsContainer.jsx
@@ -1,4 +1,7 @@
-import React from "react";import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const StyledDiv = styled.div`
   display: flex;
@@ -17,73 +20,78 @@ const StyledDiv = styled.div`
       font-size: 14px;
     }
     p {
-      border-bottom: 1px solid #AAA;
+      border-bottom: 1px solid #aaa;
       padding-bottom: 5px;
     }
-    span{
+    span {
       display: block;
       text-align: right;
       margin-top: 3px;
       font-size: 13px;
     }
   }
-`
+`;
 
 // FIXME - 결혼식 참석 여부에 따라 글자색 변경
 // 참석 = 파란색 / 불참석 = 빨간색
 
-
 function AdminAlarmListsContainer() {
-    return (
-        <StyledDiv>
-            <div>
-                <h3>참석여부 알림</h3>
-                <div className="item">
-                    <p>김미경님이 결혼식 참석에 체크하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-                <div className="item">
-                    <p>김미경님이 결혼식 불참석에 체크하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-                <div className="item">
-                    <p>김미경님이 결혼식 참석에 체크하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-                <div className="item">
-                    <p>김미경님이 결혼식 참석에 체크하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-                <div className="item">
-                    <p>김미경님이 결혼식 불참석에 체크하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-            </div>
-            <div>
-                <h3>후원 알림</h3>
-                <div className="item">
-                    <p>지현철님이 일반 세탁기에  100,000원을 후원하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-                <div className="item">
-                    <p>지현철님이 일반 세탁기에  100,000원을 후원하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-                <div className="item">
-                    <p>지현철님이 일반 세탁기에  100,000원을 후원하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-                <div className="item">
-                    <p>지현철님이 일반 세탁기에  100,000원을 후원하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-                <div className="item">
-                    <p>지현철님이 일반 세탁기에  100,000원을 후원하셨습니다.</p>
-                    <span>2023/02/06   12:10</span>
-                </div>
-            </div>
-        </StyledDiv>
-    )
+  const tempToken = import.meta.env.VITE_TEMPTOKEN;
+  const fetchAlarmData = async () => {
+    const { data } = await axios.get(
+      "http://ec2-54-180-191-154.ap-northeast-2.compute.amazonaws.com:8081/admin/alarm",
+      {
+        headers: {
+          Authorization: "Bearer " + tempToken,
+        },
+      }
+    );
+    return data.data;
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["alarmData"],
+    queryFn: fetchAlarmData,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <StyledDiv>
+      <div>
+        <h3>참석여부 알림</h3>
+        {data.attendance?.map((i) => (
+          <div key={i.userId} className="item">
+            <p>
+              {i.name}님이 결혼식 {i.attend}에 체크하셨습니다.
+            </p>
+            <span>
+              {i.date} {i.time}
+            </span>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h3>후원 알림</h3>
+        {data.donation?.map((i) => (
+          <div key={i.goodsDonationId} className="item">
+            <p>
+              {i.name}님이 {i.goods}에 {i.amount}원을 후원하셨습니다.
+            </p>
+            <span>
+              {i.date} {i.time}
+            </span>
+          </div>
+        ))}
+      </div>
+    </StyledDiv>
+  );
 }
 
-export default AdminAlarmListsContainer
+export default AdminAlarmListsContainer;
