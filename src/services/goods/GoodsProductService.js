ce1b85dc34e0b2f82e7b9922c8e8a2aae51d19e3
rondido <rppr001@naver.com>
2023-08-11 15:53:30 +0900

    [refac/WEDPRO-124] refac: 상품 등록 페이지 api 분리

--- src/services/goods/GoodsProductService.js
+++ src/services/goods/GoodsProductService.js
@@ -1,7 +1,7 @@
 import HttpClient from "../../apis/HttpClient";
 
 //상품 등록 게시판 생성
-export async function addBorderId() {
+async function addBorderId() {
   try {
     const res = await HttpClient.get("usersgoods/add/board");
     return res.data;
@@ -9,3 +9,79 @@ export async function addBorderId() {
     console.error(e);
   }
 }
+
+//상품 전체 조회
+async function getGoodsProductList() {
+  try {
+    const response = await HttpClient.get(`usersgoods/all`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//상품 등록
+async function postGoodsProductApi(url) {
+  try {
+    const response = await HttpClient.post(`usersgoods/add/product`, {
+      url: url,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//상품 삭제
+async function deleteGoods(userGoodsId) {
+  try {
+    const response = await HttpClient.delete(
+      `usersgoods?usersGoodsId=${userGoodsId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//상품이름 수정
+
+async function updateGoodsname(usersGoodsId, usersGoodsName) {
+  try {
+    const response = await HttpClient.post(
+      `usersgoods/name/update?usersGoodsId=${usersGoodsId}`,
+      {
+        usersGoodsName: usersGoodsName,
+      }
+    );
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//후원가 수정
+async function updateGoodsPrice(usersGoodsId, usersGoodsPrice) {
+  try {
+    const response = await HttpClient.post(
+      `usersgoods/cost/update?usersGoodsId=${usersGoodsId}`,
+      {
+        usersGoodsPrice: usersGoodsPrice,
+      }
+    );
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export {
+  addBorderId,
+  getGoodsProductList,
+  postGoodsProductApi,
+  deleteGoods,
+  updateGoodsname,
+  updateGoodsPrice,
+};
